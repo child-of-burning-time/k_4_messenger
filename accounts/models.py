8dd835552419cef7f2c5382a1c059753d7805708
Sergey Sova <t56k.ezpf@gmail.com>
2024-09-02 11:23:53 +0300

    Person model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+
+class Person(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pfp = models.ImageField(upload_to='profile_pics')
+    pfp_thumbnail = ImageSpecField(source='pfp',
+                                   processors=[ResizeToFill(200, 200)],
+                                   format='JPEG',
+                                   options={'quality': 90})
+    bio = models.TextField(max_length=400, blank=True, default="I'm just a kid and life is a nightmare")
