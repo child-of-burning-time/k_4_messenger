c9126741fda2344eeaf2561f3cf7b69c4fcceb8e
Sergey Sova <t56k.ezpf@gmail.com>
2024-09-02 11:36:10 +0300

    Message model added

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from accounts.models import Person
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='receiver')
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    is_modified = models.BooleanField(default=False)
+    date_modified = models.DateTimeField(auto_now=True)
